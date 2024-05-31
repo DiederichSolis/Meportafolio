e121e7ff1d5d2ab6fb7801c55c82fd5a9693df38
DiederichSolis <sol22952@uvg.edu.gt>
2024-05-30 21:48:23 -0600

    feat: modificaciones de mi nav bar y logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
         <>
         <nav className='syle1'>
             <div className='content'>
-                <img className='logo' src='https://firebasestorage.googleapis.com/v0/b/imagenes-b9423.appspot.com/o/logo.png?alt=media&token=2a649acb-b33d-4171-8b76-a589451872af' alt="" style={{ width: '15%', height: 'auto' }} />
+                <img className='logo' src='https://firebasestorage.googleapis.com/v0/b/imagenes-b9423.appspot.com/o/Blue%20Purple%20Futuristic%20Technology%20Presentation%20(1).png?alt=media&token=24d0991a-f392-46a6-9c70-f1f2e047131e' alt="" style={{ width: '15%', height: 'auto' }} />
                 <ul>
                     <li>
                         <a className="catalogo">Home</a>
