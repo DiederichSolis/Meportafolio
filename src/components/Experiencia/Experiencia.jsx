db00881a5e8afbc0c8f78b74d70e75e592147a9e
DiederichSolis <sol22952@uvg.edu.gt>
2024-06-01 02:08:32 -0600

    feat:proceso de mejoramiento para experiencia

--- src/components/Experiencia/Experiencia.jsx
+++ src/components/Experiencia/Experiencia.jsx
@@ -4,6 +4,26 @@ import ExperienceCard from "./ExperieneCard/ExperienceCard";
 import { EXPERIENCIA_W } from '../../utils/data';
 
 const Experiencia = () =>{
+
+    const sliderRef = useRef();
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed:500,
+        slidesToShow:2,
+        slidesToScroll:1,
+        arrows:false,
+        responsive:[
+            {
+                breakpoint: 769,
+                settings:{
+                    slidesToShow:1,
+                    slidesToScroll:1,
+                },
+            },
+        ],
+    };
     return(
         <section className="exp-contenedor">
             <h5>Work Experience</h5>
--- src/utils/data.js
+++ src/utils/data.js
@@ -66,5 +66,42 @@ export const EXPERIENCIA_W=[
             
         ],
         Imagenes: "https://firebasestorage.googleapis.com/v0/b/imagenes-b9423.appspot.com/o/Blue%20Purple%20Futuristic%20Technology%20Presentation%20(4).png?alt=media&token=ee55b398-3210-4663-b5c8-55b768cc9eb5",
-    }
+    },
+
+    {
+        title:"Mobile Developer- UVG",
+        date:"Julio 2023 - Noviembre 2023",
+        resposabilidades:[
+            "Desarrollo de una aplicacion con el fin de que lo estudiantes pudieran vender y comprar lo que ellos posteaban.",
+            "Desarrollo de API para el login donde se encriptaba su user.",
+            "Implementacion de Token para mantenerse logeado por un cierto limite de tiempo.",
+            "Uso de Android Studio Code con Kotlin."
+            
+        ],
+        Imagenes: "https://firebasestorage.googleapis.com/v0/b/imagenes-b9423.appspot.com/o/Blue%20Purple%20Futuristic%20Technology%20Presentation%20(5).png?alt=media&token=8a1e8a53-bc8b-499d-a9c0-547a334c4bed",
+    },
+    {
+        title:"Proyects (Calculadora) - UVG",
+        date:"Mayo 2024 - Mayo 2024",
+        resposabilidades:[
+            "Desarrollo de una calculadora con posibildad de operaciones como: Suma, Resta, Multiplicacion, Igualdad, Division",
+            "Desarrollo para permitir la opciones de ingreso de datos por medio del teclado.",
+            "Desarrollado en Next.js",
+            "Link para visualizarlo: https://main--brilliant-cheesecake-4e881a.netlify.app/"
+            
+        ],
+        Imagenes: "https://firebasestorage.googleapis.com/v0/b/imagenes-b9423.appspot.com/o/Captura%20de%20pantalla%202024-06-01%20a%20la(s)%2000.49.19.png?alt=media&token=0622f616-fb68-409a-ac2f-88b0c1b6499f",
+    },
+    {
+        title:"Proyects (Dibujo CSS) - UVG",
+        date:"Enero 2024 - Febrero 2024",
+        resposabilidades:[
+            "Desarrollo de una replica de imagen en CSS.",
+            "Desarrollo en implementacion de conceptos basicos de CSS.",
+            "Link para visualizarlo: https://tiburoncin.lat/22952/LAB.html"
+            
+        ],
+        Imagenes: "https://firebasestorage.googleapis.com/v0/b/imagenes-b9423.appspot.com/o/Captura%20de%20pantalla%202024-06-01%20a%20la(s)%2001.30.08.png?alt=media&token=f8f3596c-9727-4f1b-bc81-a0cfaa76ac45",
+    },
+
 ]
