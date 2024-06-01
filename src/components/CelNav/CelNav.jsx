fe9f4e9f6c78100dbede72b4c37cc241217dc066
DiederichSolis <sol22952@uvg.edu.gt>
2024-06-01 12:06:51 -0600

    Remove node_modules from version control and update .gitignore

--- src/components/CelNav/CelNav.jsx
+++ src/components/CelNav/CelNav.jsx
@@ -1,41 +1,48 @@
+// src/components/CelNav/CelNav.jsx
 import React from "react";
-import "./CelNav.css"
+import "./CelNav.css";
 
-const CelNav = ({isOpen, toggleMenu}) => {
-
-    return (
-        <>
-        <div
-        className={`menu ${isOpen ? "active": ""}`}
-        onClick={toggleMenu}
-        >
-            <div className="contenedor">
-                <img className="logo" src="https://firebasestorage.googleapis.com/v0/b/imagenes-b9423.appspot.com/o/Blue%20Purple%20Futuristic%20Technology%20Presentation%20(3).png?alt=media&token=ba45b64c-aa00-4932-85ff-8b2f101a7b06" alt="" />
-                <ul>
-                    <li>
-                        <a className="catalogo">Home</a>
-                    </li>
-                    <li>
-                        <a className="catalogo">Skills</a>
-                    </li>
-                    <li>
-                        <a className="catalogo">Experience</a>
-                    </li>
-                    <li>
-                        <a className="catalogo">Contact Me</a>
-                    </li>
-
-                    <button className="contactarme" onClick={()=>{}}>
-                        Contactarme
-                    </button>
-
-                </ul>
-            </div>
-
-
+const CelNav = ({ isOpen, toggleMenu }) => {
+  return (
+    <>
+      <div className={`menu ${isOpen ? "active" : ""}`} onClick={toggleMenu}>
+        <div className="contenedor">
+          <img
+            className="logo"
+            src="https://firebasestorage.googleapis.com/v0/b/imagenes-b9423.appspot.com/o/Blue%20Purple%20Futuristic%20Technology%20Presentation%20(3).png?alt=media&token=ba45b64c-aa00-4932-85ff-8b2f101a7b06"
+            alt=""
+          />
+          <ul>
+            <li>
+              <a href="#home" className="catalogo">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#skills" className="catalogo">
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#experience" className="catalogo">
+                Experience
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="catalogo">
+                Contact Me
+              </a>
+            </li>
+            <li>
+              <button className="contactarme" onClick={() => {}}>
+                Contactarme
+              </button>
+            </li>
+          </ul>
         </div>
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
-export default CelNav
+export default CelNav;
