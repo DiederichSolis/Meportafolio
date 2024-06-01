24b993fe40fdcab559074c933c7407f547689bfc
DiederichSolis <sol22952@uvg.edu.gt>
2024-05-31 23:05:12 -0600

    feat: apartado de Experiencia

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import HomeContenedor from './components/HomeContenedor/HomeContenedor';
 import Habilidades from './components/Habilidades/Habilidades';
 import cards from './components/Habilidades/CardsHabilidad/CardsHabilidad';
+import Experiencia from './components/Experiencia/Experiencia';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
     <div className="containerH">
       <HomeContenedor />
       <Habilidades />
+      <Experiencia />
       <cards />
     </div>
     </>
---
+++ src/components/Experiencia/Experiencia.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Experiencia.css'
+
+const Experiencia = () =>{
+    return(
+        <section className="exp-contenedor">
+            <h5>Work Experience</h5>
+
+            <div className="exp-cont"></div>
+        </section>
+    )
+}
+
+export default Experiencia;
