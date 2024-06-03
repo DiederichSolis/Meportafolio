2987c2fd0d6e55c211f3de12912877a8057e3288
Diederich Solis <sol2295@uvg.edu.gt>
2024-06-03 12:53:10 -0600

    Remove node_modules from version control and update .gitignore

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -15,7 +15,7 @@ const Contacto = () =>{
                 iconURL="https://firebasestorage.googleapis.com/v0/b/imagenes-b9423.appspot.com/o/gmail.png?alt=media&token=fa3ad967-46db-4988-95a8-2de6c2beb1ac"
                 text="Diederich.josue.emidio@gmail.com"
                 />
-
+                
                 <CardContacto 
                 iconURL="https://firebasestorage.googleapis.com/v0/b/imagenes-b9423.appspot.com/o/github%20(1).png?alt=media&token=4f760d80-9c08-42cd-9c76-de3212aa874c"
                 text="https://github.com/DiederichSolis"
--- src/components/Contacto/ContactoForm/ContactoForm.jsx
+++ src/components/Contacto/ContactoForm/ContactoForm.jsx
@@ -1,21 +1,37 @@
-import React from "react";
-import './ContactoForm.css'
+import React, { useRef } from "react";
+import emailjs from 'emailjs-com';
+import './ContactoForm.css';
 
 const ContactoForm = () => {
-    return(
-        <div className="contacto-form">
-            <form>
-                <div className="name-cont">
-                    <input type="text" name="firstname" placeholder="First Name" />
-                    <input type="text" name="lastname" placeholder="Last Name" />
-                </div>
-                <input type="text" name="email" placeholder="Email" />
-                <textarea type="text" name="message" placeholder="Message" rows={3} />
+    const form = useRef();
 
-                <button>ENVIAR</button>
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_s6y5t7f', 'template_utccofz', form.current, '6_mUCgYlMrhZbY8XG')
+            .then((result) => {
+                console.log(result.text);
+                alert('Mensaje enviado correctamente!');
+                form.current.reset();  // Limpiar los campos del formulario
+            }, (error) => {
+                console.log(error.text);
+                alert('Ocurrió un error, por favor intente nuevamente.');
+            });
+    };
+
+    return (
+        <div className="contacto-form">
+            <form ref={form} onSubmit={sendEmail}>
+                <div className="name-cont">
+                    <input type="text" name="firstname" placeholder="First Name" required />
+                    <input type="text" name="lastname" placeholder="Last Name" required />
+                </div>
+                <input type="email" name="email" placeholder="Email" required />
+                <textarea name="message" placeholder="Message" rows={3} required />
+                <button type="submit">ENVIAR</button>
             </form>
         </div>
-    )
-}
+    );
+};
 
 export default ContactoForm;
